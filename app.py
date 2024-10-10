4a23d398c8c5aaf95c6b9dca98e25381127f7083
sooieese <sujinjang609@gmail.com>
2024-10-10 11:31:24 +0900

    port, requirements.txt추가

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
 from flask import Flask, request, jsonify, render_template
 import requests
-
+from dotenv import load_dotenv  
+load_dotenv()
 # Flask 애플리케이션 초기화
 app = Flask(__name__)
 
@@ -54,18 +55,19 @@ def end_shadowing():
 def end_learning():
     return render_template('end-learning.html')
 
+app.config['BACKEND_URL'] = os.getenv('BACKEND_URL')
 
 # 백엔드 API와 연동하는 예제 엔드포인트
 @app.route('/api/forward', methods=['POST'])
-def forward_to_backend():
-    backend_url = 'http://localhost:5000'  # 백엔드 URL
+def forward_to_backend(): 
+    backend_url = app.config['BACKEND_URL'] 
     data = request.json  # 요청 데이터 가져오기
     try:
-        response = requests.post(backend_url, json=data)  # 백엔드에 POST 요청 보내기
+        response = requests.post(backend_url, json=data)
         return jsonify(response.json()), response.status_code  # 백엔드 응답 반환
     except requests.exceptions.RequestException as e:
         return jsonify({'error': str(e)}), 500  # 요청 예외 발생 시 에러 반환
 
 # Flask 애플리케이션 실행
 if __name__ == '__main__':
-    app.run(port=3000, debug=True)
+    app.run(PORT=3000, debug=True)
